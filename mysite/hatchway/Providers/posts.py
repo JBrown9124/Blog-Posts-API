b88577e333cff99cf695724be7e1d16696f0f38d
biker9124 <brownjs193@gmail.com>
2021-11-01 00:07:25 -0400

    added comments and improved readme

--- mysite/hatchway/Providers/posts.py
+++ mysite/hatchway/Providers/posts.py
@@ -3,27 +3,32 @@ from typing import List, Dict, Union
 
 
 class Posts(object):
+    
     def __init__(self, sort_by: str = "id", direction: str = "asc"):
         
         self.sort_by: str = sort_by
         self.direction: str = direction
         self.posts_data: List[Dict[str, Union[str, int, List[str], float]]] = []
-
+    
+    # Remove all duplicates from our posts data based on post "id".
     def remove_duplicates(self) -> List[
         Dict[str, Union[str, int, List[str], float]]]:
+        
         
         no_duplicates_posts_data: List[
             Dict[str, Union[str, int, List[str], float]]] = \
             list({v['id']: v for v in self.posts_data}.values())
         
         return no_duplicates_posts_data
-
+    
+    # Sort by given sort_by and reverse based on given direction.
     def sort_by_value(self) -> List[
         Dict[str, Union[str, int, List[str], float]]]:
         
         no_duplicates_posts_data: List[
             Dict[str, Union[
                 str, int, List[str], float]]] = self.remove_duplicates()
+        
         sorted_posts_data: List[
             Dict[str, Union[str, int, List[str], float]]] = sorted(
             no_duplicates_posts_data,
--- mysite/hatchway/views.py
+++ mysite/hatchway/views.py
@@ -7,7 +7,7 @@ from django.views.decorators.cache import cache_page
 from requests.models import Response
 from .Providers.posts import Posts
 
-
+# /api
 def index(request):
     return HttpResponse("Hatchway API", status=200)
 
@@ -19,30 +19,36 @@ def ping(request):
     return HttpResponseServerError("CRUD operation is not supported",
                                    status=400)
 # A cache with a 15 minute timeout.
+# /api/posts
 @cache_page(60 * 15)
 def posts(request):
     if request.method == "GET":
         
+        # Check to see if they provided tags parameter.
         tags: str = request.GET.get('tags', None)
         if tags is None:
             return HttpResponseServerError("Tags parameter is required",
                                            status=400)
-
+        
+        # Check to see if they provided valid sortBy parameter.
         sort_by: str = request.GET.get('sortBy', 'id')
         accepted_sort_by_values: Set[str] = {'id', 'reads', 'likes', 'popularity'}
         if sort_by not in accepted_sort_by_values:
             return HttpResponseServerError("sortBy parameter is invalid",
                                            status=400)
-
+        
+        # Check to see if they provided valid direction parameter.
         direction: str = request.GET.get('direction', 'asc')
         accepted_direction_values: Set[str] = {'asc', 'desc'}
         if direction not in accepted_direction_values:
             return HttpResponseServerError("direction parameter is invalid",
                                            status=400)
-
+        # Split tags values from tags parameter into a list.
         tags_list: List[str] = tags.split(',')
+        # Instantiate Posts class with direction and sort_by values from query arguments.
         posts: Posts = Posts(direction=direction, sort_by=sort_by)
-
+        
+        # For every tag in our tag_list we are going to extend onto our data list in our Posts instance.
         for tag in tags_list:
             response: Response = \
                 requests.get(
@@ -52,9 +58,10 @@ def posts(request):
             json_data['posts']
             posts.posts_data += tag_posts_data
 
-        posts_data: List[Dict[str, Union[str, int, List[str], float]]] = posts.results()
+        # Return results method from our Posts instance.
+        posts_data_results: List[Dict[str, Union[str, int, List[str], float]]] = posts.results()
 
-        return JsonResponse({"posts": posts_data})
+        return JsonResponse({"posts": posts_data_results})
 
     return HttpResponseServerError("CRUD operation is not supported",
                                    status=400)
